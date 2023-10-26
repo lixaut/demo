2d0ff6049f12fdc70f9135de7bfdf9c7900aef69
wangli <1309606929@qq.com>
2023-10-26 10:32:44 +0800

    init content

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+};
+
+module.exports = config;
